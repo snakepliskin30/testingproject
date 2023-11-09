2ccdc1fb00bc4b2ed2838dc8249cc1d95e4e3785
Rod <snakepliskin30@gmail.com>
2023-11-10 00:45:00 +0800

    check husky

--- src/components/sum.spec.ts
+++ src/components/sum.spec.ts
@@ -1,7 +1,7 @@
-import { sum } from './sum';
+import { sum } from "./sum";
 
-it('suming 5 and 2 will return 7', () => {
+it("suming 5 and 2 will return 7", () => {
   const a: number = 5;
   expect(a).toBe(5);
-                expect(sum(5, 2)).toBe(7);
+  expect(sum(5, 2)).toBe(7);
 });
--- src/components/sum.ts
+++ src/components/sum.ts
@@ -1,3 +1,4 @@
 export const sum = (a: number, b: number) => {
-  return a + b;
+  const c = 5;
+  return c + a + b;
 };
